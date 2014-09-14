05fb186c05d9fea54d8b6e824f3b63e4254b8a4e
Mon ouïe <mon.ouie@gmail.com>
2014-09-14 11:09:09 +0200

    Add tests for ansi_print

--- test/coolline_test.rb
+++ test/coolline_test.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 require File.expand_path("helpers.rb", File.dirname(__FILE__))
 
 context "a user uses colors for live code highlighting" do
@@ -27,4 +28,31 @@ context "a user uses non-tty input" do
   denies(:readline).raises_kind_of Exception
   asserts(:readline).equals "p 82"
   asserts(:readline).nil
+end
+
+context "printing part of a string" do
+  setup {
+    $stdout = StringIO.new
+    extend Coolline::ANSI
+  }
+
+  context "with ASCII characters" do
+    hookup { ansi_print("Hello World", 2, 9) }
+    asserts { $stdout.string }.equals "llo Wor"
+  end
+
+  context "with non-ASCII characters" do
+    hookup { ansi_print("Hellô Wörld", 3, 7) }
+    asserts { $stdout.string }.equals "lô W"
+  end
+
+  context "with full-width characters" do
+    hookup { ansi_print("日本e本語語本語本語", 3, 9) }
+    asserts { $stdout.string }.equals "e本語"
+  end
+
+  context "with ANSI codes interleaved" do
+    hookup { ansi_print("\e[3mH\e[42mello W\e[41morld\e[0m", 2, 9) }
+    asserts { $stdout.string }.equals "\e[3m\e[42mllo W\e[41mor\e[0m"
+  end
 end
