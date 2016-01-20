9d5a1394a09fa16f8fcddca5442839d74b08759c
kvokka <root_p@mail.ru>
2016-01-20 01:36:44 +0100

    Speedup and remove shell commands. RUBY 2.0+ only!

--- lib/coolline/coolline.rb
+++ lib/coolline/coolline.rb
@@ -503,30 +503,17 @@ class Coolline
   end
 
   def read_char
-  begin
-    # save previous state of stty
-    old_state = `stty -g`
-    # disable echoing and enable raw (not having to press enter)
-    system "stty raw -echo"
-    c = STDIN.getc.chr
-    # gather next two characters of special keys
+    c = STDIN.getch
     if(c=="\e")
       extra_thread = Thread.new{
         7.times{c+= STDIN.getc.chr}
       }
       # wait just long enough for special keys to get swallowed
-      extra_thread.join(0.001)
+      extra_thread.join(0.00001)
       # kill thread so not-so-long special keys don't wait on getc
       extra_thread.kill
     end
-  rescue => ex
-    puts "#{ex.class}: #{ex.message}"
-    puts ex.backtrace
-  ensure
-    # restore previous state of stty
-    system "stty #{old_state}"
+    c
   end
-  return c
-end
 
 end
